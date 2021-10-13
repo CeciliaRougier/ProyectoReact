fd3c26ae7050fe7f09ee72722bf85ed64fc8d403
Cecilia Rougier <cecilyrougier@gmail.com>
2021-10-13 20:21:11 -0300

    Creo NavBar en JS y CSS

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+
+
+//Escribo la función NavBar
+function NavBar() {
+    return (
+        <nav>
+            <button className="btnBrand">Brand</button>
+
+            <ul>
+                <li className="btnNBItem"><a href="./components/inicio.js">Inicio</a></li>
+                <li className="btnNBItem"><a href="./components/nosotros.js">Nosotros</a></li>
+                <li className="btnNBItem"><a href="./components/productos.js">Productos</a></li>
+                <li className="btnNBItem"><a href="./components/contacto.js">Contacto</a></li>
+            </ul>
+
+        </nav>
+    );
+}
+//Exportamos la Función para poder usarla
+export default NavBar;
+
