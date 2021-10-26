3f40730934c4225c7ad102081bf9b5dbcd6bc2ce
Cecilia Rougier <cecilyrougier@gmail.com>
2021-10-25 22:43:24 -0300

    Creación de ItemCount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import ItemCount from './components/ItemCount';
 import { ItemListContainer } from './components/ItemListContainer';
 import NavBar from './components/NavBar.jsx';
 import './css/NavBar.css';
+import './css/ItemCount.css'
 
 function App() {
   return (
     <div className="App">
+
       <header>
         <NavBar />              
       </header>
+
       <ItemListContainer greeting="Gracias por comprar con nosotros."></ItemListContainer>
+      
+      <body>
+        {/* Muestra la cantidad de unidades */}
+        <ItemCount stock={5} initial={1}/>
+      </body>
     </div>
   );
 }
