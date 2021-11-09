71bf9b215585faab4ea3b8e6e82cabd7f0cde67f
Cecilia Rougier <cecilyrougier@gmail.com>
2021-11-08 22:40:59 -0300

    Desafío sincronizar counter

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,9 +1,24 @@
 
-import React from "react";
+import React, { useState } from "react";
+import ItemCount from "./ItemCount";
+
+
 
 const ItemDetail = (prod) => {
 
-    return(
+    const [ count, setCount] = useState (0)
+    //Función al apretar Agregar
+    // const clickOnAdd = (total) => {
+    //     alert(`La cantidad agregada es de ${total} unidades.`)
+    //     setCount(total)
+    // }
+    // console.log(count)
+
+    const onAdd = (cant) => {
+        setCount(cant)
+    }
+    console.log(count)
+    return (
         <div key={prod.prod.id} className="detalle-item">
             <h2>Detalle del Item: {prod.prod.title}</h2>
             <div>
@@ -12,6 +27,8 @@ const ItemDetail = (prod) => {
             <div>
                 <p>{prod.prod.description}</p>
             </div>
+
+            <ItemCount stock={5} initial={1} onAdd={onAdd} />
 
         </div>
     )
